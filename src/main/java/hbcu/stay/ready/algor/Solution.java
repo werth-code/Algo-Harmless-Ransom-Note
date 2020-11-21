7534c69fc83f2f9a143f438d7a768f40d8467729
Matt Werth <matthewwerth@gmail.com>
2020-11-21 07:48:26 -0500

    Added Second Solution Using A Single Loop & Lists

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,32 +1,50 @@
 package hbcu.stay.ready.algor;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+//import java.util.HashMap;
+import java.util.List;
 
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText) {
-        String[] magTextWords = magazineText.split(" ");
-        HashMap<String, Integer> magText = new HashMap<>();
-        for(int i = 0; i < magTextWords.length; i++) {
-            String current = magTextWords[i];
-            if(!magText.containsKey(current)) magText.put(current, 1);
-            else {
-                Integer count = magText.get(current);
-                magText.put(current, count + 1);
-            }
-        }
-        //System.out.println(magText);
-        String[] messageWords = message.split(" ");
-        for (int i = 0; i < messageWords.length; i++) {
-            String current = messageWords[i];
-            if(magText.containsKey(current)) {
-                Integer count = magText.get(current);
-                magText.put(current, count -=1);
-                if(magText.get(current) < 0) return false;
-            }
+
+        //Solution 1:
+
+        List<String> messageWords = new ArrayList<>(Arrays.asList(message.split(" ")));
+        List<String> magazineWords = new ArrayList<>(Arrays.asList(magazineText.split(" ")));
+
+        for(int i = 0; i < messageWords.size(); i++) {
+            String wordAtCurrentIndex = messageWords.get(i);
+            if (magazineWords.indexOf(wordAtCurrentIndex) != -1) magazineWords.set(magazineWords.indexOf(wordAtCurrentIndex), "");
             else return false;
         }
         return true;
+
+      //Solution 2:
+
+//        String[] magTextWords = magazineText.split(" ");
+//        HashMap<String, Integer> magText = new HashMap<>();
+//        for(int i = 0; i < magTextWords.length; i++) {
+//            String current = magTextWords[i];
+//            if(!magText.containsKey(current)) magText.put(current, 1);
+//            else {
+//                Integer count = magText.get(current);
+//                magText.put(current, count + 1);
+//            }
+//        }
+//
+//        String[] messageWords = message.split(" ");
+//        for (int i = 0; i < messageWords.length; i++) {
+//            String current = messageWords[i];
+//            if(magText.containsKey(current)) {
+//                Integer count = magText.get(current);
+//                magText.put(current, count -=1);
+//                if(magText.get(current) < 0) return false;
+//            }
+//            else return false;
+//        }
+//        return true;
     }
 }
 
